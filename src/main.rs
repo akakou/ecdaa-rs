265a81299423198ed36b9c7bdb02132a35cb4302
akakou <akakou571@gmail.com>
2021-05-06 02:17:41 +0900

    [crean] Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-#[macro_use] extern crate slice_as_array;
+#[macro_use]
+extern crate slice_as_array;
 
-
-use bls12_381::{G1Projective, G2Projective, G2Affine, Scalar, pairing};
-use rand::{thread_rng, RngCore};
+use bls12_381::{pairing, G1Projective, G2Affine, G2Projective, Scalar};
+use byteorder::{BigEndian, ByteOrder};
 use ff::Field;
 use group::{Curve, GroupEncoding};
-use sha2::{Sha256, Digest};
-use byteorder::{ByteOrder, BigEndian};
-
+use rand::{thread_rng, RngCore};
+use sha2::{Digest, Sha256};
 
 #[derive(Debug, Copy, Clone)]
 struct ISK {
     x: Scalar,
-    y: Scalar
+    y: Scalar,
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -21,13 +20,13 @@ struct IPK {
     pub ecc_y: G2Projective,
     pub c: Scalar,
     pub s_x: Scalar,
-    pub s_y: Scalar
+    pub s_y: Scalar,
 }
 
 #[derive(Debug, Copy, Clone)]
 struct Issuer {
     pub isk: ISK,
-    pub ipk: IPK
+    pub ipk: IPK,
 }
 
 struct Member {
@@ -35,7 +34,7 @@ struct Member {
 }
 
 fn gen_rand_scalar(rng: &mut impl RngCore) -> Scalar {
-    Scalar::random(rng)  
+    Scalar::random(rng)
 }
 
 fn calc_sha256_scalar(vec: &[u8]) -> Scalar {
@@ -43,7 +42,7 @@ fn calc_sha256_scalar(vec: &[u8]) -> Scalar {
     hasher.update(vec);
     let hashed = hasher.finalize().to_vec();
 
-    let mut schalar: Vec<u64> = vec![0; hashed.len()/8];
+    let mut schalar: Vec<u64> = vec![0; hashed.len() / 8];
     BigEndian::read_u64_into(&hashed, &mut schalar);
     let schalar = slice_as_array!(&schalar, [u64; 4]).expect("bad hash length");
 
@@ -51,23 +50,21 @@ fn calc_sha256_scalar(vec: &[u8]) -> Scalar {
 }
 
 impl Issuer {
-    pub fn random(rng: &mut impl RngCore) -> Self {        
+    pub fn random(rng: &mut impl RngCore) -> Self {
         let isk = Self::gen_isk(rng);
         let ipk = Self::gen_ipk(&isk, rng);
 
-        return Self {
-            isk, ipk
-        }
+        return Self { isk, ipk };
     }
 
     fn gen_isk(rng: &mut impl RngCore) -> ISK {
         let x = gen_rand_scalar(rng);
         let y = gen_rand_scalar(rng);
 
-        ISK{x, y} 
-   }
+        ISK { x, y }
+    }
 
-   fn gen_ipk(isk: &ISK, rng: &mut impl RngCore) -> IPK {
+    fn gen_ipk(isk: &ISK, rng: &mut impl RngCore) -> IPK {
         let ecc_x = G2Projective::generator() * isk.x;
         let ecc_y = G2Projective::generator() * isk.y;
 
@@ -83,7 +80,7 @@ impl Issuer {
         vec.append(ecc_x.to_bytes().as_ref().to_vec().as_mut());
         vec.append(ecc_y.to_bytes().as_ref().to_vec().as_mut());
 
-        let c= calc_sha256_scalar(&vec);
+        let c = calc_sha256_scalar(&vec);
 
         let s_x = r_x + c * isk.x;
         let s_y = r_y + c * isk.y;
@@ -93,16 +90,15 @@ impl Issuer {
             ecc_y,
             c,
             s_x,
-            s_y
+            s_y,
         }
     }
 }
 
-
 struct ProofHavingSk {
     ecc_q: G1Projective,
     c_1: Scalar,
-    s_1: Scalar
+    s_1: Scalar,
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -110,12 +106,12 @@ struct MemberCredential {
     ecc_a: G1Projective,
     ecc_b: G1Projective,
     ecc_c: G1Projective,
-    ecc_d: G1Projective
+    ecc_d: G1Projective,
 }
 
 struct ProofMemberCrendetialValid {
     c_2: Scalar,
-    s_2: Scalar
+    s_2: Scalar,
 }
 
 struct IssuerJoinProcess {
@@ -123,17 +119,17 @@ struct IssuerJoinProcess {
     n: Option<Scalar>,
     ecc_q: Option<G1Projective>,
     l_j: Option<Scalar>,
-    proof_member_credential_valid: Option<MemberCredential>
+    proof_member_credential_valid: Option<MemberCredential>,
 }
 
 impl IssuerJoinProcess {
     pub fn new(issuer: Issuer) -> Self {
-        Self { 
+        Self {
             issuer,
             n: None,
             ecc_q: None,
             l_j: None,
-            proof_member_credential_valid: None
+            proof_member_credential_valid: None,
         }
     }
 
@@ -145,8 +141,7 @@ impl IssuerJoinProcess {
     }
 
     pub fn is_proof_having_sk(&mut self, proof: &ProofHavingSk) -> bool {
-        let sum = G1Projective::generator() * proof.s_1 
-                + proof.ecc_q * (- proof.c_1);
+        let sum = G1Projective::generator() * proof.s_1 + proof.ecc_q * (-proof.c_1);
 
         let mut left = Vec::new();
 
@@ -154,7 +149,7 @@ impl IssuerJoinProcess {
         left.append(proof.ecc_q.to_bytes().as_ref().to_vec().as_mut());
         left.append(self.n.unwrap().to_bytes().as_ref().to_vec().as_mut());
 
-        let left= calc_sha256_scalar(&left);
+        let left = calc_sha256_scalar(&left);
 
         self.ecc_q = Some(proof.ecc_q);
 
@@ -163,19 +158,19 @@ impl IssuerJoinProcess {
 
     pub fn gen_member_credential(&mut self, rng: &mut impl RngCore) -> MemberCredential {
         let ecc_q = self.ecc_q.expect("verify_proof_having_sk have never run.");
-        
+
         let l_j = gen_rand_scalar(rng);
         let ecc_a = G1Projective::generator() * l_j;
         let ecc_b = ecc_a * self.issuer.isk.y;
-        let ecc_c = ecc_a * self.issuer.isk.x + 
-                ecc_q * (self.issuer.isk.x * self.issuer.isk.y * l_j);
+        let ecc_c =
+            ecc_a * self.issuer.isk.x + ecc_q * (self.issuer.isk.x * self.issuer.isk.y * l_j);
         let ecc_d = ecc_q * (l_j * self.issuer.isk.y);
 
         let member_credential = MemberCredential {
             ecc_a,
             ecc_b,
             ecc_c,
-            ecc_d
+            ecc_d,
         };
 
         self.l_j = Some(l_j);
@@ -184,10 +179,16 @@ impl IssuerJoinProcess {
         member_credential
     }
 
-    pub fn prove_member_credential_valid(&mut self, rng: &mut impl RngCore) -> ProofMemberCrendetialValid {
+    pub fn prove_member_credential_valid(
+        &mut self,
+        rng: &mut impl RngCore,
+    ) -> ProofMemberCrendetialValid {
         let msg_not_ready = "verify_proof_having_sk have never run.";
         let ecc_q = self.ecc_q.expect(msg_not_ready);
-        let proof = self.proof_member_credential_valid.as_ref().expect(msg_not_ready);
+        let proof = self
+            .proof_member_credential_valid
+            .as_ref()
+            .expect(msg_not_ready);
         let l_j = self.l_j.expect(msg_not_ready);
 
         let r_2 = gen_rand_scalar(rng);
@@ -204,10 +205,7 @@ impl IssuerJoinProcess {
 
         let s_2 = r_2 + c_2 * l_j * self.issuer.isk.y;
 
-        ProofMemberCrendetialValid {
-            c_2,
-            s_2
-        }
+        ProofMemberCrendetialValid { c_2, s_2 }
     }
 }
 
@@ -215,54 +213,50 @@ struct MemberJoinProcess {
     ipk: IPK,
     sk: Scalar,
     ecc_q: G1Projective,
-    n: Scalar
+    n: Scalar,
 }
-
 
 impl MemberJoinProcess {
     pub fn random(ipk: IPK, n: Scalar, rng: &mut impl RngCore) -> Self {
         let sk = gen_rand_scalar(rng);
         let ecc_q = G1Projective::generator() * sk;
 
-        Self {
-            ipk, 
-            sk,
-            ecc_q,
-            n
-        }
+        Self { ipk, sk, ecc_q, n }
     }
 
-    pub fn prove_haveing_sk(&self, rng: &mut impl RngCore) -> ProofHavingSk{
+    pub fn prove_haveing_sk(&self, rng: &mut impl RngCore) -> ProofHavingSk {
         let r_1 = gen_rand_scalar(rng);
         let ecc_u_1 = G1Projective::generator() * r_1;
-        
+
         let mut c_1 = Vec::new();
 
         c_1.append(ecc_u_1.to_bytes().as_ref().to_vec().as_mut());
         c_1.append(self.ecc_q.to_bytes().as_ref().to_vec().as_mut());
         c_1.append(self.n.to_bytes().as_ref().to_vec().as_mut());
 
-        let c_1= calc_sha256_scalar(&c_1);
+        let c_1 = calc_sha256_scalar(&c_1);
         let s_1 = r_1 + c_1 * self.sk;
 
         ProofHavingSk {
             ecc_q: self.ecc_q,
             c_1,
-            s_1
+            s_1,
         }
     }
 
-    pub fn is_member_credential_valid(&self, credential: &MemberCredential, proof: &ProofMemberCrendetialValid) -> bool {
+    pub fn is_member_credential_valid(
+        &self,
+        credential: &MemberCredential,
+        proof: &ProofMemberCrendetialValid,
+    ) -> bool {
         if credential.ecc_a == G1Projective::generator() {
             return false;
         }
 
         {
-            let first = G1Projective::generator() * proof.s_2 
-                + credential.ecc_b * (- proof.c_2);
+            let first = G1Projective::generator() * proof.s_2 + credential.ecc_b * (-proof.c_2);
 
-            let second = self.ecc_q * proof.s_2 
-                + credential.ecc_d * (- proof.c_2);
+            let second = self.ecc_q * proof.s_2 + credential.ecc_d * (-proof.c_2);
 
             let mut left = Vec::new();
             left.append(first.to_bytes().as_ref().to_vec().as_mut());
@@ -270,8 +264,7 @@ impl MemberJoinProcess {
             left.append(credential.ecc_b.to_bytes().as_ref().to_vec().as_mut());
             left.append(self.ecc_q.to_bytes().as_ref().to_vec().as_mut());
             left.append(credential.ecc_d.to_bytes().as_ref().to_vec().as_mut());
-            let left= calc_sha256_scalar(&left)
-            ;
+            let left = calc_sha256_scalar(&left);
             println!("{}", left == proof.c_2);
 
             if left != proof.c_2 {
@@ -279,12 +272,16 @@ impl MemberJoinProcess {
             }
         }
 
-        if pairing(&credential.ecc_a.to_affine(), &self.ipk.ecc_y.to_affine()) != pairing(&credential.ecc_b.to_affine(), &G2Affine::generator()) {
+        if pairing(&credential.ecc_a.to_affine(), &self.ipk.ecc_y.to_affine())
+            != pairing(&credential.ecc_b.to_affine(), &G2Affine::generator())
+        {
             return false;
         }
 
         let sum = credential.ecc_a + credential.ecc_d;
-        if pairing(&credential.ecc_c.to_affine(), &G2Affine::generator()) != pairing(&sum.to_affine(), &self.ipk.ecc_x.to_affine()) {
+        if pairing(&credential.ecc_c.to_affine(), &G2Affine::generator())
+            != pairing(&sum.to_affine(), &self.ipk.ecc_x.to_affine())
+        {
             return false;
         }
 
@@ -292,27 +289,26 @@ impl MemberJoinProcess {
     }
 }
 
-fn main() {
-}
+fn main() {}
 
 #[test]
 fn test() {
     let mut rng = thread_rng();
 
     let issuer = Issuer::random(&mut rng);
-    
+
     let mut issuer_join_proces = IssuerJoinProcess::new(issuer.clone());
     let n = issuer_join_proces.gen_nonce(&mut rng);
-    
+
     let member_join_proces = MemberJoinProcess::random(issuer.ipk, n, &mut rng);
-    
+
     let proof = member_join_proces.prove_haveing_sk(&mut rng);
-    let is_valid= issuer_join_proces.is_proof_having_sk(&proof);
+    let is_valid = issuer_join_proces.is_proof_having_sk(&proof);
     assert!(is_valid);
 
     let credential = issuer_join_proces.gen_member_credential(&mut rng);
     let proof = issuer_join_proces.prove_member_credential_valid(&mut rng);
 
-    let is_valid= member_join_proces.is_member_credential_valid(&credential, &proof);
+    let is_valid = member_join_proces.is_member_credential_valid(&credential, &proof);
     assert!(is_valid);
 }
