969ed67a81cf150d0715382e630a38bf6d4c21e4
akakou <akakou571@gmail.com>
2023-01-18 13:58:11 +0900

    [add] generate random scalar (Fr)

--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,17 @@
-use rand::thread_rng;
+use std::println;
+
 use alloc::vec;
 use alloc::vec::Vec;
+use rand::thread_rng;
+
+use crate::utils::rand_fr;
+
+#[test]
+fn test_random_fr() {
+    let mut rng = thread_rng();
+    let a = rand_fr(&mut rng);
+    let b = rand_fr(&mut rng);
+
+    println!("a = {}", a.get_str(10));
+    println!("b = {}", b.get_str(10));
+}
