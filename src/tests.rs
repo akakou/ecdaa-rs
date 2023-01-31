ae681a18999c268bcd403d73cd55d014c809cccd
akakou <akakou571@gmail.com>
2023-01-31 11:55:18 +0900

    [update] merge tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,42 +1,20 @@
-// use mcl_rust::G2;
-
-use crate::issuer::*;
-use crate::join::*;
-use crate::utils::*;
+use crate::{cred::*, issuer::*, req_for_join::*, req_for_join::*, signature::*, utils::*};
 
 #[test]
-fn test_gen_issuer() {
+fn test_ok() {
     initalize_mcl();
 
     let isk = ISK::random();
     let ipk = IPK::random(&isk);
-
     ipk.is_valid().unwrap();
-}
-
-#[test]
-fn test_req_join() {
-    initalize_mcl();
-
-    let isk = ISK::random();
-    let ipk = IPK::random(&isk);
 
     let m = gen_seed_for_join();
-    let req = ReqForJoin::random(&m, &ipk);
-
+    let (req, sk) = ReqForJoin::random(&m, &ipk);
     req.is_valid(&m).unwrap();
-}
-
-#[test]
-fn test_cred() {
-    initalize_mcl();
-
-    let isk = ISK::random();
-    let ipk = IPK::random(&isk);
-
-    let m = gen_seed_for_join();
-    let req = ReqForJoin::random(&m, &ipk);
 
     let cred = Credential::with_no_encryption(&req, &m, &isk);
     cred.is_valid(&ipk).unwrap();
+
+    let signature = Signature::random(&m, &sk, &cred, &ipk);
+    signature.is_valid(&m).unwrap()
 }
