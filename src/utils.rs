6074db8eec529819ebc2da9abb3f59dfaad78634
akakou <akakou571@gmail.com>
2023-02-09 15:31:32 +0900

    [add] g1 function

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use mcl_rust::{CurveType, Fr, G2};
+use mcl_rust::{CurveType, Fr, G1, G2};
 
 static mut MCL_INITIALIZED: bool = false;
 
@@ -18,8 +18,14 @@ pub fn rand_fr() -> Fr {
     fr
 }
 
+pub fn g1() -> G1 {
+    let mut g1 = G1::zero();
+    g1.set_hash_of(&[1]);
+    g1
+}
+
 pub fn g2() -> G2 {
     let mut g2 = G2::zero();
     g2.set_hash_of(&[1]);
-    return g2;
+    g2
 }
